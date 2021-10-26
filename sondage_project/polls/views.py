a3ced2210dfc551c521920ea16f32633cf7f793f
Shirown <cyril.morales.s@gmail.com>
2021-10-26 11:40:13 +0200

    add views (detail, vote, results) and their url paths in polls app

--- sondage_project/polls/views.py
+++ sondage_project/polls/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello tout le monde !")
+    return HttpResponse("Bienvenue sur le sondage !")
+
+def detail(request, question_id):
+    return HttpResponse("Vous regardez la question %s." % question_id)
+
+def results(request, question_id):
+    response = "Vous regardez le résultat de la question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vous votez sur la question %s." % question_id)
